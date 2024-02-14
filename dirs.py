e91bb325a8a96437164689901e0938e3df9b38ef
RyotaroOKabe <rytrokb@gmail.com>
2024-02-14 15:05:16 -0500

    env

---
+++ dirs.py
@@ -0,0 +1,4 @@
+homedir = '/home/rokabe/data2/generative/DiffCSP_v1'
+hydradir = '/home/rokabe/data2/generative/hydra/singlerun/'
+job_folder = '2023-12-25/diff_mp20_ab_0'
+# job_folder2 = '2023-05-18/mp_20_1'
