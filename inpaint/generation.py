dad7e051aea31097d269770971b6db78a53f3fbd
RyotaroOKabe <rytrokb@gmail.com>
2024-02-18 19:00:40 -0500

    traj

--- inpaint/generation.py
+++ inpaint/generation.py
@@ -92,12 +92,15 @@ def convert_seconds_short(sec):
     days, hours = divmod(hours, 24)
     return f"{days:02d}:{hours:02d}:{minutes:02d}:{seconds:02d}"
 
-def diffusion(loader, model, step_lr):      #TODO
+def diffusion(loader, model, step_lr, save_traj):      #TODO
 
     frac_coords = []
     num_atoms = []
     atom_types = []
     lattices = []
+    all_frac_coords = []
+    all_atom_types = []
+    all_lattices = []
     input_data_list = []
     for idx, batch in enumerate(loader):
 
@@ -108,15 +111,24 @@ def diffusion(loader, model, step_lr):      #TODO
         num_atoms.append(outputs['num_atoms'].detach().cpu())
         atom_types.append(outputs['atom_types'].detach().cpu())
         lattices.append(outputs['lattices'].detach().cpu())
+        if save_traj:
+            all_frac_coords.append(traj['all_frac_coords'].detach().cpu())
+            all_atom_types.append(traj['atom_types'].detach().cpu())
+            all_lattices.append(traj['all_lattices'].detach().cpu())
 
     frac_coords = torch.cat(frac_coords, dim=0)
     num_atoms = torch.cat(num_atoms, dim=0)
     atom_types = torch.cat(atom_types, dim=0)
     lattices = torch.cat(lattices, dim=0)
     lengths, angles = lattices_to_params_shape(lattices)
+    if save_traj: 
+        all_frac_coords = torch.cat(all_frac_coords, dim=1)
+        all_atom_types = torch.cat(all_atom_types, dim=1)
+        all_lattices = torch.cat(all_lattices, dim=1)
+        all_lengths, all_angles = lattices_to_params_shape(all_lattices)    # works for all-time outputs
 
     return (
-        frac_coords, atom_types, lattices, lengths, angles, num_atoms
+        frac_coords, atom_types, lattices, lengths, angles, num_atoms, all_frac_coords, all_atom_types, all_lattices, all_lengths, all_angles
     )
 
 class SampleDataset(Dataset):      
@@ -271,7 +283,8 @@ def main(args):
     step_lr = args.step_lr if args.step_lr >= 0 else recommand_step_lr['gen'][args.dataset]
 
     start_time = time.time()
-    (frac_coords, atom_types, lattices, lengths, angles, num_atoms) = diffusion(test_loader, model, step_lr)
+    (frac_coords, atom_types, lattices, lengths, angles, num_atoms, \
+        all_frac_coords, all_atom_types, all_lattices, all_lengths, all_angles) = diffusion(test_loader, model, step_lr, args.save_traj)
 
     if args.label == '':
         gen_out_name = 'eval_gen.pt'
@@ -287,11 +300,15 @@ def main(args):
     
     torch.save({
         'eval_setting': args,
-        'frac_coords': frac_coords,
         'num_atoms': num_atoms,
+        'frac_coords': frac_coords,
         'atom_types': atom_types,
         'lengths': lengths,
         'angles': angles,
+        'all_frac_coords': all_frac_coords,
+        'all_atom_types': all_atom_types,
+        'all_lengths': all_lengths,
+        'all_angles': all_angles,
         'time': run_time,
     }, model_path / gen_out_name)
       
@@ -305,7 +322,13 @@ if __name__ == '__main__':
     parser.add_argument('--num_batches_to_samples', default=20, type=int)
     parser.add_argument('--batch_size', default=500, type=int)
     parser.add_argument('--label', default='')
-    
+
+
+    # parser.add_argument('--n_step_each', default=100, type=int) #?
+    # parser.add_argument('--min_sigma', default=0, type=float)   #!
+    parser.add_argument('--save_traj', default=False, type=bool)    #!
+
+
     parser.add_argument('--bond_sigma_per_mu', default=0.1)  
     parser.add_argument('--known_species', default=['Mn', 'Fe', 'Co', 'Ni', 'Ru', 'Nd', 'Gd', 'Dy'])  
     parser.add_argument('--arch', default=['kagome']) # 'kagome', 'honeycomb', 'triangular' 
