fc18d560d9f4743e6d841176c805ae067596815f
RyotaroOKabe <rytrokb@gmail.com>
2024-03-27 15:40:02 -0400

    savefig

--- dirs.py
+++ dirs.py
@@ -2,5 +2,6 @@ homedir = '/home/rokabe/data2/generative/DiffCSP_v1'
 hydradir = '/home/rokabe/data2/generative/hydra/singlerun/'
 job_folder = '2023-12-25/diff_mp20_ab_0'
 ehull_pred_path = '/path/to/ehull_prediction'
+ehull_pred_name = 'yymmdd-hhmmss'   # ehull classification model name
 
 # job_folder2 = '2023-05-18/mp_20_1'
