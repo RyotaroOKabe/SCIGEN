098f6c39d40aca83c9bc6b90115debc847a2224a
RyotaroOKabe <rytrokb@gmail.com>
2024-09-29 22:26:21 -0400

    screening

--- screening/utils/utils_0.py
+++
@@ -1,7 +0,0 @@
-
-
-def convert_seconds_short(sec):
-    minutes, seconds = divmod(int(sec), 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    return f"{days:02d}:{hours:02d}:{minutes:02d}:{seconds:02d}"
